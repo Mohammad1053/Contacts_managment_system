272017952b7f8a58fbe22204905492ee599c750f
Mohammad Safarzadeh <mohammad.safarzadeh1053@gmail.com>
2025-08-06 01:12:52 +0330

    The contacts module updated!

--- Contacts.py
+++ Contacts.py
@@ -1,48 +1,49 @@
 # A mudule for openning menu & doing original actions like add, search, edit, delete and see contacts.
+from colorama import *
 
 Contacts = {}
 def Show_menu():
-    print("<<<Contacts Menu>>>")
-    print("1. Add contact")
-    print("2. Search contact")
-    print("3. Edit Number")
-    print("4. Delete contact")
-    print("5. All contact")
-    print("6. Exit")
+    print((Fore.Green) + "<<<Contacts Menu>>>")
+    print((Fore.RED) + "1. Add contact")
+    print((Fore.RED) + "2. Search contact")
+    print((Fore.RED) + "3. Edit Number")
+    print((Fore.RED) + "4. Delete contact")
+    print((Fore.RED) + "5. All contact")
+    print(Fore.RED) + ("6. Exit")
 
 def Add_contact():
-    name = str(input("Contact Name: "))
-    number = int(input("Phone number: "))
+    name = str(input((Fore.CYAN) + "Contact Name: "))
+    number = int(input((Fore.BLUE) + "Phone number: "))
     Contacts[name] = number
-    print("Contact added!")
+    print((Fore.Green) + "Contact added!")
 
 def Search_contact():
-    name = str(input("Search name: "))
+    name = str(input((Fore.CYAN) + "Search name: "))
     if name in Contacts:
-        print(f"{name} -> {Contacts[name]}")
+        print((Fore.GREEN) + f"{name} -> {Contacts[name]}")
     else:
-        print("Contact didn't find!")
+        print((Fore.RED) + "Contact didn't find!")
 
 def Edit_contact():
-    name = str(input("Contact name for edit: "))
+    name = str(input((Fore.CYAN) + "Contact name for edit: "))
     if name in Contacts:
-        number = int(input("New phone number: "))
+        number = int(input((Fore.BLUE) + "New phone number: "))
         Contacts[name] = number
-        print("The contact updated.")
+        print((Fore.GREEN) + "The contact updated.")
     else:
-        print("Contact didn't find!")
+        print((Fore.RED) + "Contact didn't find!")
 
 def Delete_contact():
-    name = str(input("Enter contact's name for delete: "))
+    name = str(input((Fore.CYAN) + "Enter contact's name for delete: "))
     if name in Contacts:
         del Contacts[name]
     else:
-        print("Contact not found!")
+        print((Fore.RED) + "Contact not found!")
 
 def Show_contacts():
     if Contacts:
-        print("Contacts List: ")
+        print((Fore.YELLOW) + "Contacts List: ")
         for name, number in Contacts.items():
             print(f"- {name}: {number}")
     else:
-        print("There in no one contact yet!")
+        print((Fore.RED) + "There in no one contact yet!")
